8be7038f1e4099a66d6418ecaa4706403d339a0f
terry-mctest <elauff@ncsu.edu>
2023-12-03 13:14:34 -0500

    update ui.r

--- final-project-app/ui.R
+++ final-project-app/ui.R
@@ -8,21 +8,70 @@
 #
 #SEE ALSO 6.3.1 FROM:  https://mastering-shiny.org/action-layout.html
 
+
 library(shiny)
 
 fluidPage(
+  
+  theme = bslib::bs_theme(bootswatch = "slate"),
+  #https://bootswatch.com/
+
+      
   tabsetPanel(
-    tabPanel("Import data", 
-      fileInput("file", "Data", buttonLabel = "Upload..."),
-      textInput("delim", "Delimiter (leave blank to guess)", ""),
-      numericInput("skip", "Rows to skip", 0, min = 0),
-      numericInput("rows", "Rows to preview", 10, min = 1)
+  
+    # *ABOUT* TAB
+    tabPanel("ABOUT",
+      titlePanel("BLAH BLAH BLAH"),
+      h1("this is h1"),
+      h2("this is h2"),
+      h3("this is h3"),
+      h4("h4"),
+      h5("h5"),
+      "no h",
+      br(),
+      "still no h blah blah blah",
+      br(),
+      "whatever"
     ),
-    tabPanel("Set parameters",
-             tabsetPanel(
-               tabPanel("subpanel1"),
-                tabPanel("subpanel2")
-             )),
-    tabPanel("Visualise results")
+    
+    
+    # *DATA EXPLORATION* TAB
+    tabPanel("DATA EXPLORATION",
+      tabsetPanel(
+               
+        tabPanel("Basic descriptives",
+          #titlePanel("lkjafds"),
+          sidebarLayout(
+            sidebarPanel(
+              selectInput("bd_dropdown", "Measure of interest:",
+                choices = c("fixed acidity", "volatile acidity", "citric acid",
+                          "residual sugar", "chlorides", "free sulfur dioxide",
+                          "total sulfur dioxide", "density", "pH", "sulphates",
+                          "alcohol", "quality"))
+            ),
+            mainPanel()
+          )
+        ), 
+
+        tabPanel("Associations with wine quality")
+        )
+    ),
+
+        
+    # *MODELING* TAB
+    tabPanel("MODELING",
+      tabsetPanel(
+               
+        tabPanel("Modeling Info", 
+          fileInput("file", "Data", buttonLabel = "Upload..."),
+          textInput("delim", "Delimiter (leave blank to guess)", ""),
+          numericInput("skip", "Rows to skip", 0, min = 0),
+          numericInput("rows", "Rows to preview", 10, min = 1)),
+                
+        tabPanel("Model Fitting"),
+                
+        tabPanel("Prediction")
+      )
+    )
   )
 )
